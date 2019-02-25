3b4469dde9ec4933adb3b489754f008e2533a045
Virifortissimi <contacteghosa@gmail.com>
2019-02-25 22:43:15 +0100

    first commit

---
+++ build/precache-manifest.8dbbf295e31125ed198ae4a5b6f62ec2.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "c81b5e756fe852ad5bca",
+    "url": "/static/css/main.a1387457.chunk.css"
+  },
+  {
+    "revision": "c81b5e756fe852ad5bca",
+    "url": "/static/js/main.c81b5e75.chunk.js"
+  },
+  {
+    "revision": "402102132f5e684ac209",
+    "url": "/static/css/1.99af0f2f.chunk.css"
+  },
+  {
+    "revision": "402102132f5e684ac209",
+    "url": "/static/js/1.40210213.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "bc686492d64cccb5570e19c27d37c669",
+    "url": "/index.html"
+  }
+];
